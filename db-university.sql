94229e5a6444ab9a5ab0fc329da395eb4ee2cb97
cloudhuh <claudiu.huhulia@gmail.com>
2023-07-28 16:17:18 +0200

    ex 11

--- db-university.sql
+++ db-university.sql
@@ -61,6 +61,12 @@ FROM `teachers`
 GROUP BY (office_address);
 
 -- 3. Calcolare la media dei voti di ogni appello d'esame
+
+SELECT AVG(vote) as 'media_votes', exam_id 
+FROM `exam_student`
+WHERE vote >= 18
+GROUP BY exam_id;
+
 -- 4. Contare quanti corsi di laurea ci sono per ogni dipartimento
 
 
