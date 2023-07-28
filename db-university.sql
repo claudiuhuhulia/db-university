85187e01c9e1207337d963cc8caa5615b7aeeaa1
cloudhuh <claudiu.huhulia@gmail.com>
2023-07-28 15:46:02 +0200

    ex 7

--- db-university.sql
+++ db-university.sql
@@ -38,6 +38,10 @@ FROM `degrees`
 WHERE level = 'magistrale';
 
 -- 7. Da quanti dipartimenti è composta l'università? (12)
+
+SELECT COUNT(id) AS 'total_departments' 
+FROM `departments`;
+
 -- 8. Quanti sono gli insegnanti che non hanno un numero di telefono? (50)
 
 
