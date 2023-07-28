6e6e4fdd26649134704503bfe8368425383d856c
cloudhuh <claudiu.huhulia@gmail.com>
2023-07-28 16:08:09 +0200

    ex 10

--- db-university.sql
+++ db-university.sql
@@ -56,7 +56,9 @@ GROUP BY YEAR(enrolment_date);
 
 -- 2. Contare gli insegnanti che hanno l'ufficio nello stesso edificio
 
-
+SELECT COUNT(*) as 'number_of_teachers', (office_address)  
+FROM `teachers`
+GROUP BY (office_address);
 
 -- 3. Calcolare la media dei voti di ogni appello d'esame
 -- 4. Contare quanti corsi di laurea ci sono per ogni dipartimento
