3ad4420706285fcc0e38e7aa45ceba85ca8db385
cloudhuh <claudiu.huhulia@gmail.com>
2023-08-05 21:12:39 +0200

    join ex 2

--- db-university.sql
+++ db-university.sql
@@ -82,6 +82,13 @@ ON D.`id` = S.`degree_id`
 WHERE D.`name` = 'Corso di Laurea in Economia';
 
 -- 2. Selezionare tutti i Corsi di Laurea del Dipartimento di Neuroscienze
+
+SELECT DEG.`name` , DEP.`name` 
+FROM `degrees` AS DEG
+JOIN `departments` AS DEP
+ON DEG.`department_id` = DEP.`id`
+WHERE DEP.`name` = 'Dipartimento di Neuroscienze';
+
 -- 3. Selezionare tutti i corsi in cui insegna Fulvio Amato (id=44)
 -- 4. Selezionare tutti gli studenti con i dati relativi al corso di laurea a cui sono iscritti e ilrelativo dipartimento, in ordine alfabetico per cognome e nome
 -- 5. Selezionare tutti i corsi di laurea con i relativi corsi e insegnanti
